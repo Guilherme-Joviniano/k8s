1c14ff052831e453ebb2902176f96cf86c8efc90
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2023-10-04 09:11:50 -0300

    chore: more details and config files

--- server.go
+++ server.go
@@ -3,11 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/health", Health)
+	http.HandleFunc("/members/admin", AdminRegisteredMembers)
+	http.HandleFunc("/members", RegisteredMembers)
 	http.HandleFunc("/", Greeting)
 	err := http.ListenAndServe(":8080", nil)
 	fmt.Println(err.Error())
@@ -19,7 +26,7 @@ func Greeting(w http.ResponseWriter, r *http.Request) {
 	message := struct {
 		Message string `json:"message"`
 	}{
-		Message: fmt.Sprint("Hello I'm the %s API 😁😝", name),
+		Message: fmt.Sprint("Hello I'm the ", name, " API 😁😝"),
 	}
 
 	response, err := json.Marshal(message)
@@ -27,5 +34,44 @@ func Greeting(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
+	w.Write(response)
+}
+
+func RegisteredMembers(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("data/members.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file: %s", err.Error())
+	}
+
+	fmt.Fprintf(w, "Members: %s", string(data))
+}
+func AdminRegisteredMembers(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "Admin Member: USERNAME: %s \n PASSWORD: %s", user, password)
+}
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+		return
+	}
+
+	res := struct {
+		Message  string        `json:"message"`
+		Duration time.Duration `json:"timeAlive"`
+	}{
+		Message:  "ok",
+		Duration: duration,
+	}
+
+	response, _ := json.Marshal(res)
+
+	w.WriteHeader(200)
 	w.Write(response)
 }
